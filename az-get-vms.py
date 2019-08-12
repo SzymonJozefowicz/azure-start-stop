e91c3a9fdf395d5d79339bc0b111c5f0daf49b62
Szymon Józefowicz <szymon@statoilfuelretail.com>
2019-08-12 23:28:51 +0200

    Update az-get-vms.py

--- az-get-vms.py
+++ az-get-vms.py
@@ -1,5 +1,6 @@
 from msrestazure.azure_active_directory import MSIAuthentication
 from azure.mgmt.resource import ResourceManagementClient, SubscriptionClient
+import json
 
 # Create MSI Authentication
 credentials = MSIAuthentication()
@@ -8,36 +9,55 @@ credentials = MSIAuthentication()
 # Create a Subscription Client
 subscription_client = SubscriptionClient(credentials)
 subscription = next(subscription_client.subscriptions.list())
-print(subscription)
+#print(subscription)
 
 subscription_id = subscription.subscription_id
 
-subscription_name=""
-i=0
+subscription_name = ""
+rg_counter = 0
+vm_counter = 0
 
+#For all subscriptions
 for subscriptions in subscription_client.subscriptions.list():
     print(subscriptions.display_name)
-
-    subscription_name=subscriptions.display_name
-    subscription_id = subscriptions.subscription_id
+    subscription_name = subscriptions.display_name
+    subscription_id  = subscriptions.subscription_id
 
 # Create a Resource Management client
     resource_client = ResourceManagementClient(credentials, subscription_id)
-    
+
+
 # List resource groups as an example. The only limit is what role and policy are assigned to this MSI token.
     for resource_group in resource_client.resource_groups.list():
+        rg_counter+=1
         resource_group_name=resource_group.name
-        print(resource_group_name + " " + subscription_name)
+        #print("Resource group: Counter:" + str(rg_counter) + " Name: " + resource_group_name + " Subscription: " + subscription_name)
 
     #if you like to scan all resources
     #for resource in resource_client.resources.list():
        
     #using filters to limit resource to one resource type
+       
     #Storage account
     #for resource in resource_client.resources.list(filter="resourceType eq 'Microsoft.Storage/storageAccounts'"):
     
-    #Virtual machines
+    #Filter only virtual machines
     for resource in resource_client.resources.list(filter="resourceType eq 'Microsoft.Compute/virtualMachines'"):
-        i+=1
-        print(str(i) + ":" + resource.name + " " + resource.type)
-        print(resource)
+
+        vm_counter+=1
+        print("Virtual Machine No: " + str(vm_counter) + " Name: " + resource.name + " Resource Group: " + resource.type)
+        tags = resource.tags
+        #print(tags)
+        
+        if tags == None:
+            print "No tags"
+        else:
+
+            if "Application" in tags:
+                print("Application:" + tags["Application"])
+            if "Owner" in tags:
+                print("Owner:" + tags["Owner"])
+            if "Environment" in tags:
+                print("Environment:" + tags["Environment"])
+            if "Project" in tags:
+                print("Project:" + tags["Project"])
