8a21476b36eeac1ec9cde33aab0281b72e4dd997
Szymon Józefowicz <szymon@statoilfuelretail.com>
2019-08-12 23:40:53 +0200

    Update az-get-vms.py

--- az-get-vms.py
+++ az-get-vms.py
@@ -17,7 +17,17 @@ subscription_name = ""
 rg_counter = 0
 vm_counter = 0
 
-#For all subscriptions
+vm_auto_start = False
+vm_auto_stop = False
+vm_premium_ssd = False
+vm_start_time = ""
+vm_stop_time = ""
+vm_auto_chain = ""
+vm_start_order = 0
+vm_stop_order = 0
+
+
+#Lookup in all subscriptions
 for subscriptions in subscription_client.subscriptions.list():
     print(subscriptions.display_name)
     subscription_name = subscriptions.display_name
@@ -26,12 +36,11 @@ for subscriptions in subscription_client.subscriptions.list():
 # Create a Resource Management client
     resource_client = ResourceManagementClient(credentials, subscription_id)
 
-
 # List resource groups as an example. The only limit is what role and policy are assigned to this MSI token.
-    for resource_group in resource_client.resource_groups.list():
-        rg_counter+=1
-        resource_group_name=resource_group.name
-        #print("Resource group: Counter:" + str(rg_counter) + " Name: " + resource_group_name + " Subscription: " + subscription_name)
+    #for resource_group in resource_client.resource_groups.list():
+    #    rg_counter+=1
+    #    resource_group_name=resource_group.name
+    #    print("Resource group: Counter:" + str(rg_counter) + " Name: " + resource_group_name + " Subscription: " + subscription_name)
 
     #if you like to scan all resources
     #for resource in resource_client.resources.list():
@@ -50,7 +59,7 @@ for subscriptions in subscription_client.subscriptions.list():
         #print(tags)
         
         if tags == None:
-            print "No tags"
+            print("No tags")
         else:
 
             if "Application" in tags:
